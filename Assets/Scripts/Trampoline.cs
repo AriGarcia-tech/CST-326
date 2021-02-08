3ac963bf133675febb7fe795c114f000473bb5ca
Ari Garcia <71861098+AriGarcia-tech@users.noreply.github.com>
2021-02-07 23:29:32 -0800

    Add files via upload

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+  
+  public float impulseStrength = 1f;
+
+  void OnCollisionEnter(Collision collision)
+  {
+    Debug.Log($"{this.name} collided with the {collision.gameObject.name}");
+    //float mag = collision.relativeVelocity.magnitude;
+    Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+    rb.AddForce((transform.up * impulseStrength), ForceMode.Impulse);
+  }
+
+
+    void FixedUpdate()
+    {
+        impulseStrength = Input.GetAxisRaw(axisName);
+        GetComponent<Rigidbody>().velocity
+    }
+}
